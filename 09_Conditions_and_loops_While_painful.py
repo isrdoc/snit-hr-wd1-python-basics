ec7693bafacb3be626c2754891a8cca90040e085
Igor Srdoc <igor@srdoc.si>
2021-02-18 19:34:26 +0100

    Add For loop with fancy attemps print

--- 09_Conditions_and_loops_While_painful.py
+++ 09_Conditions_and_loops_While_painful.py
@@ -1,20 +1,13 @@
 secret = 22
-guess = 0
-number_of_attempts = 3
+max_attempts = 3
 
-has_user_guessed = False
-is_number_of_attempts_reached = False
-
-while (not has_user_guessed) and (not is_number_of_attempts_reached):
-    print("Attempts left: " + str(number_of_attempts))
-    number_of_attempts = number_of_attempts - 1
-    is_number_of_attempts_reached = number_of_attempts == 0
-
+for attempt in range(max_attempts):
+    print("Attempt number: " + str(attempt + 1) + " of " + str(max_attempts))
     guess = int(input("Guess the secret number (between 1 and 30): "))
-    has_user_guessed = guess == secret
 
     if guess == secret:
-        print("You guessed it - congratulations! It's number 22.")
+        print("You've guessed it - congratulations! It's number 22.")
+        break
     else:
         print("Sorry, your guess is not correct... The secret number is not " + str(guess))
 
