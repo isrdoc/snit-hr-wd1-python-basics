3593844d6a598cabb7d8cbb102d67be63ca491bf
Igor Srdoc <igor@srdoc.si>
2021-02-21 06:23:42 +0100

    Modify secret_number to use random intiger and hint for smaller/bigger

--- 09_Conditions_and_loops_While_painful.py
+++ 09_Conditions_and_loops_While_painful.py
@@ -1,20 +1,27 @@
-secret = 22
-attempts = 3
+import random
+
+secret = random.randint(1, 30)
+attempts = 10
 
 while True:
     print("Attempts left: " + str(attempts))
     attempts -= 1
     guess = int(input("Guess the secret number (between 1 and 30): "))
 
-    if attempts <= 0:
-        print("Out of attempts, WHY???")
-        continue
+    if attempts == 0:
+        print("Out of attempts")
+        break
 
     if guess == secret:
         print("You've guessed it - congratulations! It's number 22.")
         break
 
     print("Sorry, your guess is not correct... The secret number is not " + str(guess))
-    print("End of iteration")
 
-print("End of loop")
+    if guess > secret:
+        print("Try something smaller")
+        continue
+
+    print("Try something bigger")
+
+print("End")
