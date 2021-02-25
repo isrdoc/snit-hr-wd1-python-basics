fdf635f9db7fbdd66ed4d0103a3b334a8b7c55f3
Igor Srdoc <igor@srdoc.si>
2021-02-25 20:40:01 +0100

    Add attempts and time now to high scores

--- 11_List_and_Dictionary.py
+++ 11_List_and_Dictionary.py
@@ -1,22 +1,33 @@
 import random
 import json
+import datetime
 
 secret = random.randint(1, 30)
 attempts = 0
 
 with open("score_list.json", "r") as score_file:
     score_list = json.loads(score_file.read())
-    score_list.sort()
-    print("Top scores: " + str(score_list[:3]))
+    top_scores_message = "Top scores: "
+    number_of_top_scores = 3
+    top_three_scores = score_list[:number_of_top_scores]
+
+    for index, score in enumerate(top_three_scores):
+        top_scores_message += str(score["attempts"])
+        if index + 1 < number_of_top_scores:
+            top_scores_message += ", "
+    print(top_scores_message)
 
 while True:
     guess = int(input("Guess the secret number (between 1 and 30): "))
     attempts += 1
 
     if guess == secret:
-        print("You've guessed it - congratulations! It's number " + str(secret))
-        print("Attempts needed: " + str(attempts))
-        score_list.append(attempts)
+        current_time = datetime.datetime.now()
+        score_list.append({
+            "attempts": attempts,
+            "date": str(current_time)
+        })
+
         with open("score_list.json", "w") as score_file:
             score_file.write(json.dumps(score_list))
         break
